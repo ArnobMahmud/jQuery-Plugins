2b4a0190fc12059cee6cb9819c5e46d1e9675adf
ArnobMahmud <arnob.tech.me@gmail.com>
2020-12-25 11:40:37 +0600

    LightBox plugins slider added

---
+++ LightBox/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".gallery").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+});
+
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true,
+});
